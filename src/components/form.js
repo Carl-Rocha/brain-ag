f572fe53e1e4549bb4fcc635cdc9afb8bc3c97b4
Carl-Rocha <cajurevi@gmail.com>
2023-03-07 15:29:08 -0300

    add yup

--- src/components/form.js
+++ src/components/form.js
@@ -1,27 +1,94 @@
-import React, { useRef } from "react"
-import { useDispatch } from "react-redux"
-import { addProducer } from "../redux/actions/producer"
- 
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { addProducer } from "../redux/actions/producer";
+import * as Yup from 'yup';
+
+
 const Form = () => {
-    const dispatch = useDispatch()
-    const cpf = useRef()
-    const name = useRef()
-    const handleSave =() => {
-        dispatch(addProducer({
-            cpf:cpf.current.value,
-            name:name.current.value
-        })) 
-    } 
-    
-    return (
-        <div>
-            Produtor
-            <input ref={cpf} type="text" />
-            <input ref={name} type="text" />
-            <button onClick={handleSave}>
-                Salvar
-            </button>
-        </div>
+  const dispatch = useDispatch();
+  const name = useRef();
+  const cpf = useRef();
+  const city = useRef();
+  const state = useRef();
+  const farmTotal = useRef();
+  const arableLand = useRef();
+  const vegetationArea = useRef();
+  const crops = useRef();
+
+  const schema = Yup.object().shape({
+    cpf: Yup.string().length(11, 'CPF deve ter exatamente 11 caracteres').required('CPF é obrigatório'),
+    name: Yup.string().required('Nome é obrigatório'),
+    city: Yup.string().required('Cidade é obrigatória'),
+    state: Yup.string().required('Estado é obrigatório'),
+    farmTotal: Yup.number().min(0, 'Área total da fazenda deve ser um número maior ou igual a 0').required('Área total da fazenda é obrigatória'),
+    arableLand: Yup.number().min(0, 'Área agricultável deve ser um número maior ou igual a 0').required('Área agricultável é obrigatória'),
+    vegetationArea: Yup.number().min(0, 'Área de vegetação deve ser um número maior ou igual a 0').required('Área de vegetação é obrigatória'),
+    crops: Yup.string().required('Culturas plantadas é obrigatório'),
+    sum: Yup.number().test('sum', 'A soma de área agrícultável e vegetação não pode ser maior que a área total da fazenda', function(value) {
+      return value <= this.parent.farmTotal;
+    })
+  });
+
+  const handleSave = () => {
+    schema.validate({
+      cpf: cpf.current.value,
+      name: name.current.value,
+      city: city.current.value,
+      state: state.current.value,
+      farmTotal: farmTotal.current.value,
+      arableLand: arableLand.current.value,
+      vegetationArea: vegetationArea.current.value,
+      crops: crops.current.value,
+      sum: Number(arableLand.current.value) + Number(vegetationArea.current.value)
+    }).then(() => {
+      dispatch(addProducer({
+        cpf:cpf.current.value,
+        name:name.current.value,
+        city:city.current.value,
+        state:state.current.value, 
+        farmTotal:farmTotal.current.value,
+        arableLand:arableLand.current.value, 
+        vegetationArea:vegetationArea.current.value,
+        crops:crops.current.value 
+      }));
+    }).catch((err) => {
+      alert(err.message);
+    });
+  };
+
+  return (
+    <form>
+      Produtor:
+
+      <div>
+        Nome: <input ref={name} type="text" />
+      </div>
+      <div>
+        CPF: <input ref={cpf} size="11" type="number" />
+      </div>
+      <div>
+        Cidade: <input ref={city} type="text" />
+      </div>
+      <div>
+        Estado: <input ref={state} type="text" />
+      </div>
+      <div>
+        Área total em hectares da fazenda: <input ref={farmTotal} type="number" />
+      </div>
+      <div>
+        Área agricultável em hectares: <input ref={arableLand} type="number" />
+      </div>
+      <div>
+        Área de vegetação em hectares: <input ref={vegetationArea} type="number" />
+      </div>
+      <div>
+        Culturas plantadas (Soja, Milho, Algodão, Café, Cana de Açucar): <input ref={crops} type="text" />
+      </div>
+      <button onClick={handleSave}>
+        Salvar
+      </button>
+    </form>
+
     )
 
 } 
--- src/components/list.js
+++ src/components/list.js
@@ -8,6 +8,12 @@ const List = ({item}) => {
     const dispatch = useDispatch()
     const cpf = useRef()
     const name = useRef()
+    const city = useRef()
+    const state = useRef()
+    const farmTotal = useRef()
+    const arableLand = useRef()
+    const vegetationArea = useRef()
+    const crops = useRef()
     const handleRemove =(cpf) => {
         dispatch(removeProducer(
             cpf
@@ -18,14 +24,26 @@ const List = ({item}) => {
             id,
             {
                 cpf:cpf.current.value,
-                name:name.current.value
+                name:name.current.value,
+                city:city.current.value,
+                state:state.current.value, 
+                farmTotal:farmTotal.current.value,
+                arableLand:arableLand.current.value, 
+                vegetationArea:vegetationArea.current.value,
+                crops:crops.current.value
             }
             
         )) 
     } 
     return <div>
-            <input ref={cpf} defaultValue = { item.cpf }/>
-            <input ref={name} defaultValue = { item.name }/>
+          Nome  <input ref={name} defaultValue = { item.name }/> <br/>
+          CPF <input ref={cpf} defaultValue = { item.cpf }/><br/>
+          Cidade <input ref={city} defaultValue = { item.city }/>  <br/>
+          Estado <input ref={state} defaultValue = { item.state }/>   <br/>
+          Fazenda<input ref={farmTotal} defaultValue = { item.farmTotal }/><br/>
+          Area<input ref={arableLand} defaultValue = { item.arableLand }/>  <br/>
+          Vegetacao<input ref={vegetationArea} defaultValue = { item.vegetationArea }/>   <br/> 
+          cultura<input ref={crops} defaultValue = { item.crops }/>  <br/>
             <div onClick={()=>handleRemove(item.cpf)}>X</div>
             <button onClick={()=>handleUpdate(item.cpf)}>Atualizar</button>
     </div>
